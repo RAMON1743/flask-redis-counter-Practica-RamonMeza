9ba0a7e16e71615c43b8cf5ca1c4a38509fe1703
ramon <ramon.meza83@gmail.com>
2025-03-13 18:05:10 +0100

    Corrigiendo conexión a Redis en CircleCI y Flask

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,9 +1,7 @@
-"""Módulo de conexión con Redis."""
-
 import os
 from redis import Redis
 
 def get_redis_client():
     """Retorna una instancia del cliente Redis."""
-    redis_host = os.getenv("REDIS_HOST", "localhost")
+    redis_host = os.getenv("REDIS_HOST", "redis")  # Se usa 'redis' como nombre del servicio en CircleCI
     return Redis(host=redis_host, port=6379, db=0, decode_responses=True)
