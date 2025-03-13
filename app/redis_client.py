796bab0216414d797d8e08b3f50da4992979a283
ramon <ramon.meza83@gmail.com>
2025-03-13 18:28:05 +0100

    Corrigiendo docstring y conversión de REDIS_PORT en redis_client.py

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,8 +1,12 @@
+"""
+Módulo para la conexión con Redis.
+"""
+
 import os
 from redis import Redis
 
 def get_redis_client():
     """Retorna una instancia del cliente Redis."""
     redis_host = os.getenv("REDIS_HOST", "redis")  # Se usa "redis" en entornos Docker/CircleCI
-    redis_port = int(os.getenv("REDIS_PORT", 6379))  # Asegurar que usa el puerto correcto
+    redis_port = int(os.getenv("REDIS_PORT", "6379"))  # Convertir a int explícitamente
     return Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
