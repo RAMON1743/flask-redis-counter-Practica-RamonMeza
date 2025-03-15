dacca7a04cc99217c27123f6cdd19d2322a1cdc4
ramon <ramon.meza83@gmail.com>
2025-03-15 21:22:41 +0100

    Corrección en redis_client.py: ajustada línea larga para pylint

--- app/redis_client.py
+++ app/redis_client.py
@@ -6,6 +6,11 @@ from redis import Redis
 def get_redis_client():
     """Retorna una instancia del cliente Redis."""
     redis_host = os.getenv("REDIS_HOST", "redis-server")
-    redis_port = os.getenv("REDIS_PORT", "6379")  # Mantiene el valor como str y lo convierte después
+    redis_port = os.getenv("REDIS_PORT", "6379")  # Convertir a int después
 
-    return Redis(host=redis_host, port=int(redis_port), db=0, decode_responses=True)
+    return Redis(
+        host=redis_host,
+        port=int(redis_port),
+        db=0,
+        decode_responses=True
+    )
